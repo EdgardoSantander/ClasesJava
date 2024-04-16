e1f2d80d50a3fe027c7bb6333f31287b205d499e
Edgar SA <Garko1209@hotmail.com>
2024-04-16 10:29:24 -0600

    creacion de clase principal para llamar a gerente

---
+++ test/TestEmpleado.java
@@ -0,0 +1,11 @@
+package test;
+import recursos.*;
+
+public class TestEmpleado {
+    public static void main(String [] args){
+        Gerente gerente = new Gerente("Edgardo",1500,"Desarrollo");
+
+
+        gerente.obtenerDetalles();
+    }
+}
